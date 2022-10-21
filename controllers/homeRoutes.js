0508aed776aca96be535fac142279b71635baa90
terrence <terrence.budnik@gmail.com>
2022-10-21 18:07:20 -0500

    login and signup saves user

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,23 +1,54 @@
 const router = require('express').Router();
 const { Post, User } = require('../models');
+const withAuth = require('../utils/auth');
 
 
-router.get('/', async (req, res) => {
-    res.render('all-posts');
-  
-});
+
+  router.get('/', async (req, res) => {
+    try {
+      const postData = await Post.findAll({
+        include: [User]
+      });
+      const posts = postData.map((post) => post.get({ plain: true }));
+      res.render("all-posts", { posts });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 
 router.get('/login', async (req, res) => {
   res.render('login');
 });
 
-router.get('/dashboard', async (req, res) => {
-  res.render('dashboard');
+
+router.get('/dashboard', withAuth, async (req, res) => {
+    res.render("all-posts-admin", {
+      layout: "dashboard",
+    });
 });
 
 
 router.get('/signup', async (req, res) => {
   res.render('signup');
 });
+
+// try {
+//   const postData = await Post.findAll({
+//     where: {
+//       user_id: req.session.user_id
+//     }
+//   });
+//   const posts = postData.map((post) => post.get({ plain: true }));
+//   res.render("all-posts-admin", {
+//     layout: "dashboard",
+//     posts
+//   });
+// }
+// catch (err) {
+//   res.redirect("login");
+// }
+// });
+
 
 module.exports = router;
