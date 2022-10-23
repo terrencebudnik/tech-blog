e59d92f9d07fad6ba3c8f22f563f5e2f3b69516c
terrence <terrence.budnik@gmail.com>
2022-10-23 16:10:44 -0500

    started delete routes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -19,24 +19,24 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
-// router.delete('/:id', withAuth, async (req, res) => {
-//   try {
-//     const projectData = await Project.destroy({
-//       where: {
-//         id: req.params.id,
-//         user_id: req.session.user_id,
-//       },
-//     });
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Project.destroy({
+      where: {
+        post_id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
 
-//     if (!projectData) {
-//       res.status(404).json({ message: 'No project found with this id!' });
-//       return;
-//     }
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
 
-//     res.status(200).json(projectData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
--- public/js/new.js
+++ public/js/new.js
@@ -1,44 +1,48 @@
 const postBtn = $("#post-btn");
+const delBtn = $("#delete-btn")
 
 
-postBtn.on('click', newPost);
+
 
 function newPost(event) {
-    event.preventDefault();
-    const postTitle = $("#post-title");
-    const postContent = $("#post-content");
-    const newPost = {
-        title: postTitle.val(),
-        content: postContent.val()
-        };
-    fetch('/api/posts', {
-        method: "POST",
-        body: JSON.stringify(newPost),
-        headers: { 'Content-Type': 'application/json' }
-    }).then(response => response.json())
-        .then(response => {
-            if (response.ok) {
-                console.log('Post successful')
-                document.location.replace('/dashboard');
-              } else {
-                alert('Failed to create post'); 
-              }
-        })
+  event.preventDefault();
+  const postTitle = $("#post-title");
+  const postContent = $("#post-content");
+  const newPost = {
+    title: postTitle.val(),
+    content: postContent.val()
+  };
+  fetch('/api/posts', {
+    method: "POST",
+    body: JSON.stringify(newPost),
+    headers: { 'Content-Type': 'application/json' }
+  }).then(response => response.json())
+    .then(response => {
+      if (response.ok) {
+        console.log('Post successful')
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to create post');
+      }
+    })
 
-    }; 
+};
 
-    const delButtonHandler = async (event) => {
-        if (event.target.hasAttribute('data-id')) {
-          const id = event.target.getAttribute('data-id');
-      
-          const response = await fetch(`/api/posts/${id}`, {
-            method: 'DELETE',
-          });
-      
-          if (response.ok) {
-            document.location.replace('/dashboard');
-          } else {
-            alert('Failed to delete post');
-          }
-        }
-      };
+const delPost = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to delete post');
+    }
+  }
+};
+
+postBtn.on('click', newPost);
+delBtn.on('click', delPost);
